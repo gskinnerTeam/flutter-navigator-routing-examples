4efbb0877de4e7d04bc97ab9d39ebdfae7552476
Shawn <shawn@shawnblais.com>
2020-11-28 10:43:21 -0700

    Add deep-link support

--- lib/example_declarative_basic/declarative_nav_basic.dart
+++ lib/example_declarative_basic/declarative_nav_basic.dart
@@ -1,66 +1,94 @@
+import 'package:back_button_interceptor/back_button_interceptor.dart';
 import 'package:flutter/material.dart';
+import 'package:flutter/services.dart';
 
-import 'shared_pages.dart';
+import '../shared_pages.dart';
 
 //TODO: Add a fade transition
 //TODO: Add manual browser sync and deeplink!??
 
-// Declare navModel as a package level variable for demo purposes
-// Normally you would pass this around with GetIt or Provider
+// SystemNavigator.routeUpdated(routeName: routeName, previousRouteName: _lastAnnouncedRouteName
+
+// Declare navModel as a package level variable for demo purposes (normally you would pass this around with GetIt, Provider etc
 NavModel _navModel = NavModel();
 
-class DeclarativeNavBasic extends StatelessWidget {
+String _prevPath;
+void syncSystemPath(String value) {
+  if (value != _prevPath) {
+    _prevPath = value;
+    SystemNavigator.routeUpdated(routeName: value ?? "/", previousRouteName: _prevPath ?? "/");
+  }
+}
+
+String get initialSystemPath => WidgetsBinding.instance.window.defaultRouteName;
+
+class DeclarativeNavBasic extends StatefulWidget {
+  // Some btn handlers, that update the navModel > triggering the AnimatedBuilder > which rebuilds Navigator.pages
+  @override
+  _DeclarativeNavBasicState createState() => _DeclarativeNavBasicState();
+}
+
+class _DeclarativeNavBasicState extends State<DeclarativeNavBasic> {
   void _handleCategorySelected(String categoryId) => _navModel.selectedCategory = categoryId;
   void _handleProductSelected(String productId) => _navModel.selectedProduct = productId;
+  void _handleSignInPressed() => _navModel.isSignedIn = true;
+  void _handleSignOutPressed() => _navModel.clear();
+
+  @override
+  void initState() {
+    super.initState();
+    // Get the initial deeplink from web / device(???)
+    _navModel.parseDeeplink(initialSystemPath);
+    // Hook system BACK event into our app state, this is primarily for Android
+    BackButtonInterceptor.add((_, __) => _navModel.tryGoBack());
+    // Listen for nav changes, and browser know we've changed
+    //_navModel.addListener(() => syncBrowserLocation(_navModel.currentPath));
+  }
 
   @override
   Widget build(BuildContext context) {
     return MaterialApp(builder: (_, __) {
-      return AnimatedBuilder(
+      return Scaffold(
+        body: AnimatedBuilder(
           animation: _navModel,
           builder: (_, __) {
+            //Whenever we rebuild, sync the system path
+            syncSystemPath(_navModel.currentPath);
+
             return Navigator(
-              pages: _buildNavigatorPages(),
-              onPopPage: _handlePagePopped,
+              // Compose list of Page widgets, that represent our current App state
+              pages: [
+                // Show an authPage if needed
+                if (!_navModel.isSignedIn) ...{
+                  AuthPage(_handleSignInPressed),
+                }
+                // If we're signed in, show other pages according to state
+                else ...{
+                  CategoryListPage(_handleCategorySelected, _handleSignOutPressed),
+                  if (_navModel.isCategorySelected) ...{
+                    ProductsListPage(_navModel._selectedCategory, _handleProductSelected),
+                  },
+                  if (_navModel.isProductSelected) ...{
+                    DetailsPage(_navModel._selectedProduct),
+                  }
+                },
+                // Map the list of views, to a list of Pages, as required by Navigator
+              ].map((view) => MaterialPage(child: view)).toList(),
+
+              /// Called when [pop] is invoked and the current [Route] corresponds to a [Page] found in the [pages] list.
+              onPopPage: (Route route, dynamic result) {
+                // Allow top page-route to handle pop()
+                if (route.didPop(result)) {
+                  syncSystemPath(_navModel.currentPath);
+                }
+                return _navModel.tryGoBack();
+                // Otherwise, handle it in our own nav model and return the results
+              },
             );
-          });
+          },
+        ),
+      );
     });
-  }
-
-  // Build a list of Page widgets, that represent our current App state
-  // Pages are just a Widget that is tied to a PageRoute
-  List<Page> _buildNavigatorPages() {
-    return [
-      CategoriesPage(_handleCategorySelected),
-      if (_navModel.isCategorySelected) ...{
-        ProductsPage(
-          _handleProductSelected,
-          _navModel._selectedCategory,
-        ),
-      },
-      if (_navModel.isProductSelected) ...{
-        ProductDetailsPage(
-          _navModel._selectedProduct,
-        ),
-      }
-      // Map the list of views, to a list of Pages, as required by Navigator
-    ].map((view) => MaterialPage(child: view)).toList();
-  }
-
-  bool _handlePagePopped(Route<dynamic> route, dynamic result) {
-    if (route.didPop(result)) return true; // Give the route a chance to handle the Pop
-    // If we have a selected product, move back to category.
-    if (_navModel.selectedProduct != null) {
-      _navModel.selectedProduct = null;
-      return true;
-    }
-    // If we have a selected category, move back to home
-    else if (_navModel.selectedCategory != null) {
-      _navModel.selectedCategory = null;
-      return true;
-    }
-    // Can't move back anymore, allow the app to be popped
-    return false;
   }
 }
 
@@ -76,8 +104,57 @@ class NavModel extends ChangeNotifier {
   set selectedCategory(String value) => change(() => _selectedCategory = value);
   bool get isCategorySelected => _selectedCategory != null;
 
+  bool _isSignedIn = true;
+  bool get isSignedIn => _isSignedIn;
+  set isSignedIn(bool value) => change(() => _isSignedIn = value);
+
+  void clear() {
+    isSignedIn = false;
+    selectedCategory = null;
+    selectedProduct = null;
+  }
+
+  bool tryGoBack() {
+    // Cant go back if we're not signed in
+    if (!isSignedIn) return false;
+    // If we have a selected product, move back to category.
+    if (_navModel.selectedProduct != null) {
+      _navModel.selectedProduct = null;
+      return true;
+    }
+    // If we have a selected category, move back to home
+    else if (_navModel.selectedCategory != null) {
+      _navModel.selectedCategory = null;
+      return true;
+    }
+    // Can't move back anymore, allow the app to be popped
+    return false;
+  }
+
   void change(VoidCallback cmd) {
     cmd();
     notifyListeners();
+  }
+
+  String get currentPath {
+    String path = "/";
+    if (isCategorySelected) {
+      path += "$selectedCategory/";
+      if (isProductSelected) {
+        path += "$selectedProduct/";
+      }
+    }
+    return path;
+  }
+
+  void parseDeeplink(String deeplinkPath) {
+    List<String> segments = deeplinkPath.split("/")..removeWhere((e) => e == "");
+    _selectedCategory = _selectedProduct = null;
+    if (segments.length > 0) {
+      selectedCategory = segments[0];
+      if (segments.length > 1) {
+        selectedProduct = segments[1];
+      }
+    }
   }
 }
---
+++ lib/shared_pages.dart
@@ -0,0 +1,149 @@
+import 'package:flutter/material.dart';
+
+ButtonStyle get _btnStyle => ButtonStyle(
+      backgroundColor: MaterialStateProperty.all(Colors.grey.shade300),
+      minimumSize: MaterialStateProperty.all(Size(200, 40)),
+      padding: MaterialStateProperty.all(EdgeInsets.symmetric(horizontal: 20, vertical: 20)),
+    );
+
+class CategoryListPage extends StatelessWidget {
+  const CategoryListPage(this.onCategorySelected, this.onLogoutPressed, {Key key}) : super(key: key);
+  final VoidCallback onLogoutPressed;
+  final void Function(String) onCategorySelected;
+  @override
+  Widget build(BuildContext context) {
+    return Scaffold(
+      body: Center(
+        child: Column(
+          children: [
+            SizedBox(height: 40),
+            TextButton(onPressed: onLogoutPressed, child: Text("LOGOUT"), style: _btnStyle),
+            SizedBox(height: 40),
+            Text("Choose a category...", style: TextStyle(fontSize: 25)),
+            Flexible(
+                child: SingleChildScrollView(
+              child: Wrap(
+                alignment: WrapAlignment.center,
+                children: [
+                  ...[
+                    "Toys",
+                    "Appliances",
+                    "Tools",
+                    "Food",
+                  ].map((e) {
+                    return _CardButton(
+                      label: e,
+                      onPressed: () => onCategorySelected(e),
+                    );
+                  }).toList()
+                ],
+              ),
+            )),
+          ],
+        ),
+      ),
+    );
+  }
+}
+
+class ProductsListPage extends StatelessWidget {
+  const ProductsListPage(this.selectedCategory, this.onProductSelected, {Key key}) : super(key: key);
+  final void Function(String) onProductSelected;
+
+  final String selectedCategory;
+
+  @override
+  Widget build(BuildContext context) {
+    return Scaffold(
+      body: Center(
+        child: Column(children: [
+          SizedBox(height: 40),
+          _BackBtn(),
+          Text(selectedCategory, style: TextStyle(fontSize: 30)),
+          Flexible(
+            child: SingleChildScrollView(
+              child: Wrap(
+                children: [
+                  "Product1",
+                  "Product2",
+                  "Product3",
+                  "Product4",
+                  "Product5",
+                  "Product6",
+                  "Product7",
+                  "Product8",
+                ].map((e) {
+                  return _CardButton(
+                    onPressed: () => onProductSelected(e),
+                    label: e,
+                  );
+                }).toList(),
+              ),
+            ),
+          ),
+        ]),
+      ),
+    );
+  }
+}
+
+class DetailsPage extends StatelessWidget {
+  const DetailsPage(this.selectedProduct, {Key key}) : super(key: key);
+  final String selectedProduct;
+
+  @override
+  Widget build(BuildContext context) {
+    return Scaffold(
+      body: Center(
+        child: Column(
+          children: [
+            SizedBox(height: 40),
+            _BackBtn(),
+            Text(selectedProduct, style: TextStyle(fontSize: 30)),
+          ],
+        ),
+      ),
+    );
+  }
+}
+
+class AuthPage extends StatelessWidget {
+  const AuthPage(this.onSigninPressed, {Key key}) : super(key: key);
+  final VoidCallback onSigninPressed;
+
+  @override
+  Widget build(BuildContext context) {
+    return Scaffold(
+      body: Center(
+        child: Column(
+          mainAxisSize: MainAxisSize.min,
+          children: [_CardButton(onPressed: onSigninPressed, label: "Sign In")],
+        ),
+      ),
+    );
+  }
+}
+
+class _BackBtn extends StatelessWidget {
+  const _BackBtn({Key key}) : super(key: key);
+  @override
+  Widget build(BuildContext context) {
+    return TextButton(onPressed: () => Navigator.of(context).pop(), child: Text("<< Back"));
+  }
+}
+
+class _CardButton extends StatelessWidget {
+  const _CardButton({Key key, this.onPressed, this.label}) : super(key: key);
+  final VoidCallback onPressed;
+  final String label;
+
+  @override
+  Widget build(BuildContext context) {
+    return Card(
+        child: SizedBox(
+      width: 150,
+      height: 150,
+      child: TextButton(onPressed: onPressed, child: Text(label), style: _btnStyle),
+    ));
+  }
+}
