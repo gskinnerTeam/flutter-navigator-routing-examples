66c39a35150884be8789fd0f62fbc5e4c85d1826
Shawn <shawn@shawnblais.com>
2020-11-29 00:13:27 -0700

    Fix imports

--- lib/main.dart
+++ lib/main.dart
@@ -1,6 +1,4 @@
 import 'package:flutter/material.dart';
-import 'package:navigator2_tests/example_declarative_basic/declarative_nav_with_deeplink.dart';
-import 'example_declarative_basic/simple_declarative_nav.dart';
 import 'examples/simple_imperative_nav.dart';
 
 void main() {
