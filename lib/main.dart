ad378cd3357a0c8833c73ae75880fe59fc401604
Shawn <shawn@shawnblais.com>
2020-11-29 00:05:54 -0700

    Organize

--- lib/main.dart
+++ lib/main.dart
@@ -1,7 +1,7 @@
 import 'package:flutter/material.dart';
 import 'package:navigator2_tests/example_declarative_basic/declarative_nav_with_deeplink.dart';
 import 'example_declarative_basic/simple_declarative_nav.dart';
-import 'example_declarative_basic/simple_imperative_nav.dart';
+import 'examples/simple_imperative_nav.dart';
 
 void main() {
   runApp(SimpleImperativeNav());
